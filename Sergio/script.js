775c60b73e8c69af71514f79c54aad090db5b7fd
Sérgio Melo <srique7@gmail.com>
2023-09-30 11:47:44 -0300

    criacao de formulario DOM

--- Sergio/script.js
+++ Sergio/script.js
@@ -26,14 +26,14 @@ addClienteTable = async (dados) => {
             <tr>
                 <th scope="row">${i}</th>
                 <td>${cliente.name}</td> 
-                <td>${cliente.email}</td> 
+                <td id="email-${i}">${cliente.email}</td> 
                 <td>${cliente.address}</td> 
                 <td>${cliente.city}</td> 
                 <td>${cliente.state}</td> 
                 <td>${cliente.cep}</td> 
                 <td>${cliente.phoneNumber}</td>
                 <td>
-                    <button id="edita-${i}" onclick="editar(this)" type="button" class="btn btn-primary">Editar</button>
+                    <button id="editar-${i}" onclick="editar(this)" type="button" class="btn btn-primary">Editar</button>
                     
                     <button id="deletar-${i}" onclick="deletar(this)" type="button" class="btn btn-danger">Deletar</button>
                 </td>
@@ -58,6 +58,46 @@ buscar = async () => {
     await addClienteTable(filter_clientes);
 }
 
+showModal = () => {
+    let meuModal = new bootstrap.Modal(document.getElementById("infor"));
+    meuModal.show();
+}
+
 deletar = async (e) => {
     console.log(e.id);
+
+   showModal();
+}
+
+editar = async (e) => {
+    const ID = parseInt(e.id.split("editar-")[1]);
+    const infor = await window.clientes;
+    
+// filtrar pelo Email
+    const EMAIL_INFOR = document.getElementById(`email-${ID}`).innerHTML;
+    let filter_clientes = infor.filter((cliente) =>
+    cliente.email === EMAIL_INFOR
+    );
+
+    
+
+    const NAME = document.getElementById("infor-name");
+    const EMAIL = document.getElementById("infor-email");
+
+    NAME.value = filter_clientes[0].name;
+    EMAIL.value = filter_clientes[0].email;
+
+    const ENVIAR = document.getElementById("enviar")
+    ENVIAR.innerHTML = "Alterar";
+    ENVIAR.setAttribute("data-type" , "editar");
+
+    const TITLE = document.getElementById("infor-title")
+    TITLE.innerHTML = `Editar: ${filter_clientes[0].name}`
+
+
+
+
+    showModal();
+    
+    
 }
